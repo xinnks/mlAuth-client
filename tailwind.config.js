bca863b5c12658e14ae1c1165845c17864908999
James Sinkala <xinnks@gmail.com>
2022-06-30 23:26:39 +0300

    feat: Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+        primary: {
+          DEFAULT: '#f9e80f',
+          50: '#fefde6',
+          100: '#fdface',
+          200: '#fcf69c',
+          300: '#fbf16a',
+          400: '#faec38',
+          500: '#c7b905',
+          600: '#958b04',
+          700: '#645c02',
+          800: '#322e01',
+          900: '#191801',
+        }
+      }
+    },
   },
   plugins: [],
 }
