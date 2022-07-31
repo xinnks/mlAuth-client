44e6a38e3a1c117e7d9463f50719b1748a05ba3e
James Sinkala <xinnks@gmail.com>
2022-07-31 23:17:29 +0300

    feat: Add public pages layout

---
+++ src/layouts/publicPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import publicNavigation from './../components/publicNavigation.vue';
+import publicFoot from './../components/publicFoot.vue';
+import {useRoute} from "vue-router"
+
+const route = useRoute()
+
+const routesWithPageTitles = () => ["dashboard","settings"].indexOf(route.name.toLowerCase()) !== -1
+</script>
+
+<template>
+  <div class="gradient relative">
+    <public-navigation></public-navigation>
+    <slot></slot>
+    <public-foot></public-foot>
+  </div>
+</template>
+
+<style scoped>
+body{
+  font-family: 'Source Sans Pro', sans-serif;
+}
+</style>
