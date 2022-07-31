edc7045c0f3165449d1a3d21adaf82fc3518a7a9
James Sinkala <xinnks@gmail.com>
2022-07-31 23:28:35 +0300

    docs: Add missing text

--- src/views/home.vue
+++ src/views/home.vue
@@ -17,7 +17,7 @@ import PublicPage from "./../layouts/publicPage.vue";
             User authentication can be time consuming and messy, and that's only for one project.
           </p>
           <p class="leading-normal text-2xl mb-8">
-            Focus on the other parts of your project and let authenticate your users.
+            Focus on the other parts of your project and let us authenticate your users.
           </p>
           <router-link to="/register" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
             Get Started
